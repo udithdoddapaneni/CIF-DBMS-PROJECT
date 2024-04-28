5cd5872d5863b7a6eb06ab711ab4e3db5bebebe6
LVL_UP <126270067+udithdoddapaneni@users.noreply.github.com>
2024-04-28 23:50:44 +0530

    Update API.py

--- Main-backend/API.py
+++ Main-backend/API.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI, Response, Request
+from fastapi.middleware.cors import CORSMiddleware
+from BaseModels import LoginCredentials, Token
+from hashlib import sha256
 import database_handler
 
 
 
 app = FastAPI()
 
+
 users: dict[str, (str, str)] = {}
+
+# Allow requests from specific origins
+origins = [
+    "http://127.0.0.1:5500",
+]
+
+# Configure CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],  # Allow specific methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 # db: database_handler.PostgresqlDB = None
@@ -24,40 +42,44 @@ def authenticate(username: str, password: str):
     try:
         db = database_handler.login(username, password)
         db = None
-        users[username] = (username, password)
         return True
     except:
         return False
 
 @app.post("/login")
-async def func(username: str, password: str,response : Response):
+async def login(credentials: LoginCredentials ,response : Response):
+    username, password = credentials.username, credentials.password
+    token = sha256(username.encode()).hexdigest()
     if authenticate(username, password):
-        response.set_cookie(key = username, value = "user_session", httponly=True)
-        return {"message" : "Cookie is set on the browser"}
+        users[token] = (username, password)
+        return {"message" : "Cookie is set on the browser", "Token": token}
     else:
         return {"message" : "Failed"}
 
-@app.get("/logout")
-async def func(response: Response, request: Request):
+@app.post("/logout")
+async def logout(token: Token):
     try:
-        session = list(request.cookies)[0]
-        if session:
-            response.delete_cookie(key=session)
+        # session = list(request.cookies)[0]
+        session = token.token
+        user = users[session][0]
+        if session in users:
             users.pop(session)
-            return {"message": f"Logout successful for session: {session}"}
+            return {"message": f"Logout successful for user: {user}"}
         else:
             return {"message": "No active session"}
-    except:
-        return {"message: No active session"}
+    except Exception as err:
+        print(err)
+        return {"message": "No active session"}
 
-@app.get("/current_user")
-async def show_current_user(request: Request):
+@app.post("/current_user")
+async def show_current_user(token: Token):
     try:
-        current_user = list(request.cookies)[0]
+        # current_user = list(request.cookies)[0]
+        current_user = token.token
         db = open_connection(current_user)
         result = list(db.execute_dql_commands("select current_user"))
         db = None # dereference
-        return result[0][0]
-
-    except Exception as err: 
-        print("error", err)
+        return {"message":result[0][0]}
+    except Exception as err:
+        print("error_show_user", err)
+        return {"message":"NO CURRENT USER"}
