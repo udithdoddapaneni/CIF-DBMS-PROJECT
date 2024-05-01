ef5e253a27fa05ea49faabfbc85a4c17acb035f7
LVL_UP <126270067+udithdoddapaneni@users.noreply.github.com>
2024-05-01 21:10:47 +0530

    Update BaseModels.py

--- Main-backend/BaseModels.py
+++ Main-backend/BaseModels.py
@@ -9,7 +9,15 @@ class Token(BaseModel):
     token: str
 
 class Equipment(BaseModel):
+    token: str
     name: str
 
 class Slot_Request(BaseModel):
-    ID: str
+    token: str
+    slot_ID: int
+    project_ID: str
+
+class Decision(BaseModel):
+    token: str
+    request_id: int
+    decision: str
