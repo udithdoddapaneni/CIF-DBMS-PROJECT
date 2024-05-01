b582f9e062a9798bb62f868f24f12bb7d3128358
LVL_UP <126270067+udithdoddapaneni@users.noreply.github.com>
2024-05-01 21:10:33 +0530

    Update database_handler.py

--- Main-backend/database_handler.py
+++ Main-backend/database_handler.py
@@ -149,24 +149,31 @@ def show_all_equipment(db: PostgresqlDB):
 
 def show_avaiable_slots_for_equipment(db: PostgresqlDB, equipment_name: str):
     query = f"select s.slot_id, equipment.equipment_name, s.equipment_id, s.slot_time from equipment, check_slots() as s where s.equipment_id = equipment.equipment_id and equipment.equipment_name = '{equipment_name}'"
-    result = list(db.execute_dql_commands(query))
+    r = list(db.execute_dql_commands(query))
+    result = []
+    fields = ["slot_id", "equipment_name", "equipment_id", "slot_time"]
+    for i in r:
+        record = {}
+        for j in range(len(i)):
+            record[fields[j]] = i[j]
+        result.append(record)
     return result
 
 def request_a_slot_for_project(db: PostgresqlDB, slot_id: int, project_id: str):
     query = f"call request_slot({slot_id}, '{project_id}')"
-    list(db.execute_ddl_and_dml_commands(query))
+    db.execute_ddl_and_dml_commands(query)
 
 def decide_by_super_visor(db: PostgresqlDB, request_id: int, decision: str):
     query = f"call decide_by_super_visor({request_id}, '{decision}')"
-    list(db.execute_ddl_and_dml_commands(query))
+    db.execute_ddl_and_dml_commands(query)
 
 def decide_by_faculty_incharge(db: PostgresqlDB, request_id: int, decision: str):
     query = f"call decide_by_faculty_incharge({request_id}, '{decision}')"
-    list(db.execute_ddl_and_dml_commands(query))
+    db.execute_ddl_and_dml_commands(query)
 
 def decide_by_staff_incharge(db: PostgresqlDB, request_id: int, decision: str):
     query = f"call decide_by_staff_incharge({request_id}, '{decision}')"
-    list(db.execute_ddl_and_dml_commands(query))
+    db.execute_ddl_and_dml_commands(query)
 
 def show_requests_supervisor(db: PostgresqlDB):
     query = "select * from show_requests_supervisor()"
@@ -180,24 +187,62 @@ def show_requests_supervisor(db: PostgresqlDB):
         result.append(record)
     return result
 
+def show_requests_faculty_incharge(db: PostgresqlDB):
+    query = "select * from show_requests_faculty_incharge()"
+    r = list(db.execute_dql_commands(query))
+    result = []
+    fields = ["request_id", "equipment_name", "slot_time", "slot_id"]
+    for i in r:
+        record = {}
+        for j in range(len(i)):
+            record[fields[j]] = i[j]
+        result.append(record)
+    return result
+
+def show_requests_staff_incharge(db: PostgresqlDB):
+    query = "select * from show_requests_staff_incharge()"
+    r = list(db.execute_dql_commands(query))
+    result = []
+    fields = ["request_id", "equipment_name", "slot_time", "slot_id"]
+    for i in r:
+        record = {}
+        for j in range(len(i)):
+            record[fields[j]] = i[j]
+        result.append(record)
+    return result
+
 def is_member_of(db: PostgresqlDB):
-    user = list(db.execute_dql_commands("select current_user"))[0]
+    user = list(db.execute_dql_commands("select current_user"))[0][0]
     group = "students"
     query = f"select is_member_of('{user}', '{group}')"
-    result = list[db.execute_dql_commands(query)][0]
+    result = list(db.execute_dql_commands(query))[0][0]
     if result:
         return group
     
     group = "faculty"
     query = f"select is_member_of('{user}', '{group}')"
-    result = list[db.execute_dql_commands(query)][0]
+    result = list(db.execute_dql_commands(query))[0][0]
     if result:
         return group
     
     group = "staff"
     query = f"select is_member_of('{user}', '{group}')"
-    result = list[db.execute_dql_commands(query)][0]
+    result = list(db.execute_dql_commands(query))[0][0]
     if result:
         return group
     
     return None
+
+def show_requests_student(db: PostgresqlDB):
+    user = list(db.execute_dql_commands("select current_user"))[0][0]
+    view = "request_"+user
+    query = f"select {view}.request_id, {view}.slot_id, slot.equipment_id, {view}.proj_id, slot.slot_time from {view}, slot where slot.slot_id = {view}.slot_id"
+    r = list(db.execute_dql_commands(query))
+    result = []
+    fields = ["request_id", "slot_id", "equipment_id", "proj_id", "slot_time"]
+    for i in r:
+        record = {}
+        for j in range(len(i)):
+            record[fields[j]] = i[j]
+        result.append(record)
+    return result
